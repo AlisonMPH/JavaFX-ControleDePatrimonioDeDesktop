56a6633ce41d23a202161a9b669bcde0401a987c
AlisonMPH <alisonmozer@gmail.com>
2022-06-12 18:07:19 -0300

    Primeiro

---
+++ src/javafx/controledepatrimoniodedesktop/controller/FXMLMenuController.java
@@ -0,0 +1,70 @@
+package javafx.controledepatrimoniodedesktop.controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+
+public class FXMLMenuController implements Initializable {
+
+    @FXML
+    private MenuItem menuCadastrosDesktop;
+
+    @FXML
+    private MenuItem menuCadastrosAlocar;
+
+    @FXML
+    private MenuItem menuMostrarGrafico;
+
+    @FXML
+    private MenuItem menuMostrarRelatorio;
+
+    @FXML
+    private MenuItem menuMostrarRelatorioQuantidade;
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    @FXML
+    public void showFXMLMenuCadastrosDesktop() throws IOException {
+        System.out.println("\nCarregando Tela FXMLMenuCadastrosDesktop\n");
+        AnchorPane a = (AnchorPane) FXMLLoader.load(getClass().getResource("/javafx/controledepatrimoniodedesktop/view/FXMLMenuCadastrosDesktop.fxml"));
+        anchorPane.getChildren().setAll(a);
+    }
+
+    @FXML
+    public void showFXMLMenuAlocarDesktop() throws IOException {
+        System.out.println("\nCarregando Tela FXMLMenuAlocarDesktop\n");
+        AnchorPane a = (AnchorPane) FXMLLoader.load(getClass().getResource("/javafx/controledepatrimoniodedesktop/view/FXMLMenuAlocarDesktop.fxml"));
+        anchorPane.getChildren().setAll(a);
+    }
+
+    @FXML
+    public void showFXMLMenuRelatorio() throws IOException {
+        System.out.println("\nCarregando Tela FXMLMenuRelatorio\n");
+        AnchorPane a = (AnchorPane) FXMLLoader.load(getClass().getResource("/javafx/controledepatrimoniodedesktop/view/FXMLRelatorio.fxml"));
+        anchorPane.getChildren().setAll(a);
+    }
+    @FXML
+    public void showFXMLMenuRelatorioQuantidade() throws IOException {
+        System.out.println("\nCarregando Tela FXMLMenuRelatorioQuantidade\n");
+        AnchorPane a = (AnchorPane) FXMLLoader.load(getClass().getResource("/javafx/controledepatrimoniodedesktop/view/FXMLRelatorioQuantidade.fxml"));
+        anchorPane.getChildren().setAll(a);
+    }
+    @FXML
+    public void showFXMLMenuGraficoAlocacao() throws IOException {
+        System.out.println("\nCarregando Tela FXMLMenuGraficoAlocacao\n");
+        AnchorPane a = (AnchorPane) FXMLLoader.load(getClass().getResource("/javafx/controledepatrimoniodedesktop/view/FXMLGraficoAlocacao.fxml"));
+        anchorPane.getChildren().setAll(a);
+    }
+
+}
---
+++ src/javafx/controledepatrimoniodedesktop/desktop/Usuario.java
@@ -0,0 +1,34 @@
+
+package javafx.controledepatrimoniodedesktop.desktop;
+
+import java.io.Serializable;
+
+
+public class Usuario implements Serializable{
+    private int id;
+    private String nome;
+    
+    public Usuario(){
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    @Override
+    public String toString() {
+        return getNome();
+    }
+    
+}
